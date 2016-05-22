e60ef2cea9de11c434dc6d9297397f30ba8d68cb
Julian Palladino <julianpalladino@hotmail.com>
2016-05-22 18:17:49 -0300

    Adds prefix and sufix

--- exercises.pl
+++ exercises.pl
@@ -1,10 +1,18 @@
-%long(+L,-Res)
-%long([],cero).
-%long(.(_,XS), suc(Rec)) :- long(XS,Rec).
-
 long([], 0).
 long([_|T], X+1) :- long(T, X).
 
-src([X],[X]).
-src([X,X|XS],[X|RS]) :- src([X|XS],[X|RS]).
-src([X,Y|XS],[X,Y|RS]) :- src([Y|XS],[Y|RS]), X \= Y.
+removeConsecutiveRepeated([X],[X]).
+removeConsecutiveRepeated([X,X|XS],[X|RS]) :- removeConsecutiveRepeated([X|XS],[X|RS]).
+removeConsecutiveRepeated([X,Y|XS],[X,Y|RS]) :- removeConsecutiveRepeated([Y|XS],[Y|RS]), X \= Y.
+
+%prefix(+List,?Prefix)
+prefix(L1, L2) :- append(L1,_, L2).
+
+%sufix(+Lista,?Sufix)
+sufix(L1, L2) :- append(_, L1, L2).
+
+%sublist(+Lista,?Subl)
+sublist(L1, L2) :- sufix(LprefAndL1, L1), prefix(Lpref, LprefAndL1)
+
+%$Lpref | L1 = LprefAndL1
+%LprefAndL1 | Lsuf = L2
