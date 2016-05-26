3461f128eec0fa7fe4c1ed65675d16989834bca7
zappa1 <jp_rulo@hotmail.com>
2016-05-26 13:17:18 -0300

    Adds insert and permutation in prolog

--- exercises.pl
+++ exercises.pl
@@ -5,14 +5,18 @@ removeConsecutiveRepeated([X],[X]).
 removeConsecutiveRepeated([X,X|XS],[X|RS]) :- removeConsecutiveRepeated([X|XS],[X|RS]).
 removeConsecutiveRepeated([X,Y|XS],[X,Y|RS]) :- removeConsecutiveRepeated([Y|XS],[Y|RS]), X \= Y.
 
-%prefix(+List,?Prefix)
+% prefix(+List,?Prefix)
 prefix(L1, L2) :- append(L1,_, L2).
 
-%sufix(+Lista,?Sufix)
+% sufix(+Lista,?Sufix)
 sufix(L1, L2) :- append(_, L1, L2).
 
-%sublist(+Lista,?Subl)
-sublist(L1, L2) :- sufix(LprefAndL1, L1), prefix(Lpref, LprefAndL1)
+% sublist(+Lista,?Subl)
+sublist(L1, L2) :- sufix(L2, Lsuf), prefix(Lsuf,L1).
 
-%$Lpref | L1 = LprefAndL1
-%LprefAndL1 | Lsuf = L2
+% insert(?X,+L,?LwithX)
+insert(X, L, LwithX) :- append(L1, L2, L), append(L1, [X|L2], LwithX).
+
+% permutation(+L,?P)
+permutation([X | LTail], LPerm) :- permutation(LTail, LTailPerm), insert(X, LTailPerm, LPerm).
+permutation([], []).
